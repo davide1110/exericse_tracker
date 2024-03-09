473f766a58e47da9e5b4687d9587e204c33bd4e5
davide1110 <davidegrasso200068@gmail.com>
2024-03-09 22:48:48 +0100

    -add save new user method - add save new exercise method - add find log

--- index.js
+++ index.js
@@ -20,17 +20,66 @@ mongoose.connect(process.env.MONGODBURL, {}).then(
 app.post('/api/Users', (req,res) =>{
     const username = req.body.username;
     const user = new User({username: username});
-    User.create(user, (done, err) => {
-        
-    });
-    
-    
-
-  //  const user = new User({username: username});
-    //
-    res.json({username: "", _id: ""});
+    User.create(user)
+    .then(savedUser => { 
+      console.log(`New user created ${savedUser}`);
+      res.json({username: savedUser.username, _id: savedUser._id});
+    })
+    .catch(error => {
+      console.error(`Encountered error while creating a new user ${error}`)
+      res.json({error: error});
+    });    
 }); 
 
 
+/*
+{"_id":"65eccbf40b80b70013c990c9","username":"e","date":"Sat Aug 03 2024","duration":1,"description":"1"}
+*/
+app.post('/api/users/:id/exercises', (req,res) => {
+  const body = req.body;
+
+  User.findOneAndUpdate({_id: req.params.id}, {log: [{  duration: body.duration,
+    description: body.description,
+    date: body.date}]})
+  .then(savedUser => { 
+    console.log(`New Exercise created ${savedUser}`);
+    const log = savedUser.log[0];
+    res.json({ _id: savedUser._id, username: savedUser.username, date: log.date,
+    duration: log.duration, description: log.description});
+  })
+  .catch(error => {
+    console.error(`Encountered error while creating a new exercise ${error}`)
+    res.json({error: error});
+  });   
+});
+///api/users/:_id/logs?[from][&to][&limit]
+/*
+{
+  username: "fcc_test",
+  count: 1,
+  _id: "5fb5853f734231456ccb3b05",
+  log: [{
+    description: "test",
+    duration: 60,
+    date: "Mon Jan 01 1990",
+  }]
+}
+*/
+app.get('/api/users/:id/logs?', (req,res) => {
+  console.log(req.params.id);
+    const id = req.params.id.toString();
+    const from = req.query.from;
+    const to = req.query.to;
+    const limit = req.query.limit;
+    let query = User.findById(id).where({date: "08/03/2024"}).limit(limit);
+    //User.findById(id)
+    query.then(user => {
+      console.log(`found user : ${user}`);
+      res.json({user});
+    }).catch(error => {
+      console.log(`No user found with id ${id}`);
+      res.json({error: error});
+    });
+});
 
 app.use(cors({optionsSuccessStatus: 200}));
--- schema/user.js
+++ schema/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = new Schema({
-    username: String
+    username: String,
+    log: [{
+        description:String,
+        duration: Number,
+        date: String
+    }]
 });
 exports.User =  mongoose.model('User', User);
 
